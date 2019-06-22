ee68b7c23d3f5889e2e3bf17ec28d8214ddc7b0c
Luan Azevedo <azeveedolh@gmail.com>
2019-06-21 22:54:35 -0300

    Montagem do layout inicial e consulta a api rest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header/index";
+import "./styles.css";
+import Main from "./pages/main/index";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Rocketseat</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+);
 
 export default App;
